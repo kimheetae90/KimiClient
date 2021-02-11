215f44a1792404ebce13beb027b8caeb83f2b16c
kimheetae <kimheetae90@gmail.com>
2021-02-11 15:16:10 +0900

    Create Core

---
+++ Assets/Scripts/Core/GameInstance.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KimiClient
+{
+    namespace Core
+    {
+        using Utility;
+
+        public class GameInstance : MonoBehaviour
+        {
+            public static GameInstance Instance;
+            private List<ManagerBase> managerList;
+
+            protected void Awake()
+            {
+                Instance = this;
+                Initialize();
+            }
+
+            public void Initialize()
+            {
+                managerList = new List<ManagerBase>();
+            }
+
+            public bool RegistManager(ManagerBase inManager)
+            {
+                if (managerList.Contains(inManager))
+                {
+                    Debug.LogError("GameInstnace already contains ManagerBase : " + inManager);
+                    return false;
+                }
+
+                managerList.Add(inManager);
+                return true;
+            }
+
+            public bool UnRegistManager(ManagerBase inManager)
+            {
+                if (!managerList.Contains(inManager))
+                {
+                    Debug.LogError("GameInstnace doesn't contains ManagerBase : " + inManager);
+                    return false;
+                }
+
+                managerList.Remove(inManager);
+                return true;
+            }
+
+            public void PublishGameEvent<GameEventType>(GameEvent<GameEventType> inGameEvent)
+            {
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/ManagerBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KimiClient
+{
+    namespace Core
+    {
+        using Utility;
+
+        public class ManagerBase : MonoBehaviour
+        {
+
+
+            protected void Awake()
+            {
+                GameInstance.Instance.RegistManager(this);
+            }
+
+            protected void OnDestroy()
+            {
+                GameInstance.Instance.UnRegistManager(this);
+            }
+
+            public void ReceiveGameEvent<GameEventType>(GameEvent<GameEventType> inGameEvent)
+            {
+
+            }
+        }
+    }
+}
+
