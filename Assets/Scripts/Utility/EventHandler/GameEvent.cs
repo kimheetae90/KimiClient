b43ecae81bc722d4ba219aec68ce9386d84314b1
kimheetae <kimheetae90@gmail.com>
2021-02-11 14:15:45 +0900

    Finish EventHandler

---
+++ Assets/Scripts/Utility/EventHandler/GameEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KimiClient
+{
+    namespace Utility
+    {
+        public class GameEvent<GameEventType>
+        {
+            public GameEventType Type;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/EventHandler/GameEventSubscriber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KimiClient
+{
+    namespace Utility
+    {
+        public interface IEventSubscriber<GameEventType>
+        {
+            void Receive(GameEvent<GameEventType> inEvent);
+        }
+    }
+}
