be1b6d824ef562618bfd739d0c87c16122091d20
kimheetae <kimheetae90@gmail.com>
2021-03-08 08:32:30 +0900

    Remove ManagerBase

--- Assets/Scripts/Core/GameInstance.cs
+++ Assets/Scripts/Core/GameInstance.cs
@@ -10,47 +10,6 @@ namespace KimiClient
 
         public class GameInstance : MonoBehaviour
         {
-            public static GameInstance Instance;
-            private List<ManagerBase> managerList;
-
-            protected void Awake()
-            {
-                Instance = this;
-                Initialize();
-            }
-
-            public void Initialize()
-            {
-                managerList = new List<ManagerBase>();
-            }
-
-            public bool RegistManager(ManagerBase inManager)
-            {
-                if (managerList.Contains(inManager))
-                {
-                    Debug.LogError("GameInstnace already contains ManagerBase : " + inManager);
-                    return false;
-                }
-
-                managerList.Add(inManager);
-                return true;
-            }
-
-            public bool UnRegistManager(ManagerBase inManager)
-            {
-                if (!managerList.Contains(inManager))
-                {
-                    Debug.LogError("GameInstnace doesn't contains ManagerBase : " + inManager);
-                    return false;
-                }
-
-                managerList.Remove(inManager);
-                return true;
-            }
-
-            public void PublishGameEvent(GameEvent inGameEvent)
-            {
-            }
         }
     }
 }
