4bd736608025fe0b4f768a0944d72ac772bf748e
kimheetae <kimheetae90@gmail.com>
2021-03-18 08:10:48 +0900

    Overide GameMode

--- Assets/Scripts/PlayLogic/GameMode.cs
+++ Assets/Scripts/PlayLogic/GameMode.cs
@@ -6,6 +6,25 @@
     {
         public class GameMode : FSMRoot<EGameState>
         {
+            public bool Start()
+            {
+                return Run(CurrentState.ID);
+            }
+
+            public bool InitGameState(GameState inState)
+            {
+                return AddState(inState);
+            }
+
+            public bool ChangeGameState(EGameState inState)
+            {
+                return Transition(inState);
+            }
+
+            public GameState GetGameState(EGameState inState)
+            {
+                return GetState(inState) as GameState;
+            }
         }
     }
 }
